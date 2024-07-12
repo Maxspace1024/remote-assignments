f078f1346e5f01b1e9dbf3f5b3722ad2b223a8be
Brian Chen <jeb.3332@gmail.com>
2024-07-12 16:57:16 +0800

    fix sql

--- Week-4/Assignment-4/assigment-4.sql
+++ Week-4/Assignment-4/assigment-4.sql
@@ -1,4 +1,4 @@
 select article.id, title, content, user.email from assignment.article
 left join assignment.user on user.id = article.id_user;
 
-select * from assignment.article order by id limit 6 ffset 6;
+select * from assignment.article order by id limit 6 offset 6
